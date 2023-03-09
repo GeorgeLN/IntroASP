f43e95f3190474349a8b139bc9b66a07450beeb1
Jorge López <jelopez0403@gmail.com>
2023-03-09 14:59:29 -0500

    Se agrega el proyecto

---
+++ Controllers/BrandController.cs
@@ -0,0 +1,21 @@
+﻿using IntroASP.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace IntroASP.Controllers
+{
+    public class BrandController : Controller
+    {
+        private readonly PubContext _context;
+
+        public BrandController(PubContext context) //Inyección de dependencias.
+        {
+            _context = context;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Brands.ToListAsync());
+        }
+    }
+}
---
+++ Models/ViewModels/BeerViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Runtime.InteropServices;
+
+namespace IntroASP.Models.ViewModels
+{
+    public class BeerViewModel
+    {
+        [Required] //Permite hacer que el campo en el formulario sea obligatorio.
+        [Display(Name = "Nombre")] //Permite cambiar el nombre del campo a manera de información extra.
+        public String Name { get; set; }
+
+        [Required]
+        [Display(Name = "Marca")]
+        public int BrandId { get; set; }
+    }
+}
